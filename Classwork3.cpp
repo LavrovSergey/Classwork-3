05c3952752933c39db14b1c8aca18b045b069d9a
LavrovSergey <54863977+LavrovSergey@users.noreply.github.com>
2019-10-07 22:24:03 +0300

    1

---
+++ Classwork3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int sum = 0;
+	int maxi=0;
+	setlocale (LC_ALL, "Russian");
+	cout << "Введите размерность массива:" << endl;
+	int a;
+	cin >> a;
+	int* mas = new int[a];
+	cout << "Заполните массив" << endl;
+	for (int i = 0; i < a; i++)
+	    {
+		cout << "[" << i + 1 << "]" << ":";
+		cin >> mas[i];
+	    }
+	int max = mas[0];
+	for (int i = 0; i < a; i++)
+	{
+		if (mas[i] > max)
+		{
+			max = mas[i];
+		}
+
+	}
+	cout << "Максимальный елемент= " << max<<endl;
+	for (int i = 0; i < a; i++)
+	  {
+		if (mas[i] > 0)
+		{maxi = i;}
+	  }
+	for (int i=0; i<maxi; i++)
+	     {
+		sum=mas[i]+sum;
+	     }
+	cout<<"Сума елементов масива до последнего положительного= "<<sum;
+		return 0;
+} 
