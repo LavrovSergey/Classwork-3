55650123fb3f540c1b8d41fb3f8e6af394602102
LavrovSergey <54863977+LavrovSergey@users.noreply.github.com>
2019-10-08 23:09:20 +0300

    1

--- Classwork3.cpp
+++ Classwork3.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <math.h>
 using namespace std;
 int main()
 {
@@ -8,6 +9,10 @@ int main()
 	cout << "Введите размерность массива:" << endl;
 	int a;
 	cin >> a;
+	while (a < 0) { 
+		cout << "Ошибка. Должен быть больше 0."<<endl;
+		cin >> a;
+	}
 	int* mas = new int[a];
 	cout << "Заполните массив" << endl;
 	for (int i = 0; i < a; i++)
@@ -34,6 +39,41 @@ int main()
 	     {
 		sum=mas[i]+sum;
 	     }
-	cout<<"Сума елементов масива до последнего положительного= "<<sum;
-		return 0;
+	cout<<"Сума элементов массива до последнего положительного= "<<sum<<endl;
+	int n1, n2;
+	cout << "Введите интервал, и все числа, модуль которых попадает в него, удаляться. Массив сожметься и пустые ячеки заполняться нулями" << endl;
+	cout << "От ";
+	cin >> n1;
+	cout << " До ";
+	cin >> n2;
+	for (int i=0;i<a;i++)
+	{
+		if (abs(mas[i]) > n1 && abs(mas[i]) < n2)
+		{
+			mas[i] = 0;
+		}
+		else(mas[i] = mas[i]);
+	}
+	
+	for (int i = 0; i < a; i++) 
+	{
+		if (mas[i] == 0) 
+		{
+			for (int j = i; j < a - 1; j++) mas[j] = mas[j + 1];
+			mas[a - 1] = 0;
+		}
+	}
+	
+		
+	for (int i=0;i<a;i++)
+	{
+		cout << mas[i] << " ";
+	}
+	
+	
+	
+	
+	
+	return 0;
+
 } 
